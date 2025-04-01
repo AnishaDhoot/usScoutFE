c7ba8f5cbc3cdde621a8307219396eb95399f3aa
AnishaDhoot <anishadhoot26@gmail.com>
2025-04-01 17:43:34 +0530

    feat:started hero page frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,222 @@
 import Image from "next/image";
-
+import {TextHoverEffect} from "../components/ui/text-hover-effect";
+import { FloatingNav } from "../components/ui/floating-navbar";
+import { link } from "fs";
+import heropageimage from "@/app/assets/heropageimage.png"
+import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
+import round from "@/app/assets/round.png";
+import under from "@/app/assets/under.png";
+import Carousel from "../components/ui/carousel";
+import { Box, Lock, Search, Settings, Sparkles } from "lucide-react";
+import { GlowingEffect } from "@/components/ui/glowing-effect";
+import { GridItem } from "@/components/ui/glowing-effect";
 export default function Home() {
+  const navItems = [
+    {
+      name: "About us",
+      link: "/about",
+    },
+    {
+      name: "Features",
+      link: "/features",
+    },
+    {
+      name: "Use Cases",
+      link: "/usecases",
+    },
+    {
+      name: "Pricing",
+      link: "/pricing",
+    }
+  ];
+  const slideData = [
+    {
+      title: "",
+      button: "Read more",
+      src: "/assets/c1.png",
+    },
+    {
+      title: "",
+      button: "Read more",
+      src: "/assets/c2.png",
+    },
+    {
+      title: "",
+      button: "Read more",
+      src: "/assets/c3.png",
+    },
+    {
+      title: "",
+      button: "Read more",
+      src: "/assets/c4.png"
+    },
+    {
+      title: "",
+      button: "Read more",
+      src: "/assets/c5.png",
+    },
+    {
+      title: "",
+      button: "Read more",
+      src: "/assets/c6.png",
+    }
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="bg-[#1A1A1C] text-white text-center  flex flex-col items-center justify-center">
+     <FloatingNav navItems={navItems} className="lato-font" />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+     <TextHoverEffect text="UXSCOUT" duration={0.01}/> 
+    <div className=" flex flex-row items-center justify-center">
+      <div className="flex-[50%] flex flex-col color-[#F5F5F5] Fraunces-font">
+      <h1 className="Fraunces-font text-7xl/[109px] text-left ml-12 mr-12 relative z-10">
+  Optimize Your Website
+  <Image
+    src={round}
+    alt="circle"
+    className="absolute z-[-1] top-1/2 left-1/6 transform -translate-x-1/2 -translate-y-1/2"
+  />
+  , Keep Visitors Engaged
+</h1>
+
+        <h2 className=" lato-font text-3xl/[40px] text-left ml-12 mr-12 mt-4">UX Scout maps your entire site, highlights user struggles, and provides ranked solutions to boost conversions effortlessly.</h2>
+      </div>
+      <div className="flex-[50%]">
+      <CardContainer className="inter-var dark lato-font">
+      <CardBody className="bg-[#2B1C20] relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-[#2B1C20] dark:border-[#C3073F]/[0.2] border-[#C3073F]/[0.1] border-2 w-[75%] ml-2 sm:w-[30rem] h-auto rounded-xl p-8 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-2xl font-bold text-neutral-600 dark:text-white"
+        >
+          Know more about us 
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-lg max-w-sm mt-2 dark:text-neutral-300"
+        >
+        Hover over this card to unleash the power of CSS perspective Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src={heropageimage}
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            className="px-4 py-2 rounded-xl text-md font-normal dark:text-white"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-[#4E4E50] dark:text-white dark:border-white dark:border-1 text-white text-md font-bold"
           >
-            Read our docs
-          </a>
+            Sign up
+          </CardItem>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </CardBody>
+    </CardContainer>
+      </div>
     </div>
+    {/* =========================================================================== */}
+    <h1 className="Fraunces-font text-left text-7xl/[109px] text-white w-[95%] z-10 relative mb-8">
+    What UX Scout
+      <span className="ml-4"> 
+        <Image
+          src={under}
+          alt="under"
+          className="absolute z-[-1] top-[35%] left-[38%] transform -translate-x-1/3 h-[70%] w-[10%]" 
+        />
+      </span>
+      Does for You
+    </h1>
+    {/* ============================================================================= */}
+    <Carousel slides={slideData}  />
+    {/* ===================================================================================== */}
+    <h1 className="Fraunces-font text-right text-7xl/[109px] text-white w-[95%] z-10 relative mb-8 mt-12 mr-8"> How UX Scout Helps You</h1>
+    {/*=============================================*/  }
+    <ul className="grid grid-cols-1 h-full grid-rows-none gap-4 md:grid-cols-12 md:grid-rows-3 lg:gap-4   xl:grid-rows-2">
+    <GridItem
+    area="md:[grid-area:1/1/2/7] xl:[grid-area:1/1/2/5]"
+    icon={
+    <Image 
+      className="h-4 w-4 text-[#F5F5F5] dark:text-neutral-400" 
+      src="/assets/icon1.png" 
+      alt="icon 1" 
+      width={4} 
+      height={4} 
+    />
+  }
+  title="Improving Conversion Rates"
+  description="By identifying friction points on key pages (like checkout or lead forms), businesses can reduce abandonment and boost conversions."
+/>
+ 
+      <GridItem
+        area="md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]"
+        icon={
+          <Image 
+            className="h-4 w-4 text-[#F5F5F5] dark:text-neutral-400" 
+            src="/assets/icon2.png" 
+            alt="icon 1" 
+            width={4} 
+            height={4} 
+          />
+        }
+        title="Optimizing Marketing Campaigns"
+        description="Understand user interaction with landing pages to refine strategies, improve targeting, and maximize ROI on ads."
+      />
+ 
+      <GridItem
+        area="md:[grid-area:2/1/3/7] xl:[grid-area:1/5/3/8]"
+        icon={
+          <Image 
+            className="h-4 w-4 text-[#F5F5F5] dark:text-neutral-400" 
+            src="/assets/icon3.png" 
+            alt="icon 1" 
+            width={4} 
+            height={4} 
+          />
+        }
+        title="Enhancing Customer Retention"
+        description="Address user frustration by identifying pain points, improving experience, and increasing loyalty."
+      />
+ 
+      <GridItem
+        area="md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]"
+        icon={
+          <Image 
+            className="h-4 w-4 text-[#F5F5F5] dark:text-neutral-400" 
+            src="/assets/icon4.png" 
+            alt="icon 1" 
+            width={4} 
+            height={4} 
+          />
+        }
+        title="Improving SEO Performance"
+        description="Optimize website structure and content to improve search rankings, driving more organic traffic and increasing visibility."
+      />
+ 
+      <GridItem
+        area="md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]"
+        icon={
+          <Image 
+            className="h-4 w-4 text-[#F5F5F5] dark:text-neutral-400" 
+            src="/assets/icon5.png" 
+            alt="icon 1" 
+            width={4} 
+            height={4} 
+          />
+        }
+        title="Streamlining User Onboarding"
+        description="Identify and resolve friction in the onboarding process to reduce drop-offs and improve user retention."
+      />
+      </ul>
+      </div>
   );
 }
---
+++ components/ui/floating-navbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { motion, AnimatePresence } from "motion/react";
+
+export const FloatingNav = ({
+  navItems,
+  className,
+}: {
+  navItems: {
+    name: string;
+    link: string;
+    // icon?: JSX.Element;
+  }[];
+  className?: string;
+}) => {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.3 }}
+        className={cn(
+          "fixed top-0 left-0 w-full mx-auto flex items-center justify-between border border-white/[0.2] bg-[#1A1A1C] shadow-lg pr-4 pl-8 py-2 z-[5000] lato-font mb-18",
+          className
+        )}
+      >
+        {/* Centered Navigation Links */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-4">
+          {navItems.map((navItem, idx) => (
+            <Link
+              key={`link=${idx}`}
+              href={navItem.link}
+              className="relative text-white flex space-x-1 hover:text-gray-300 transition-colors"
+            >
+              <span className="hidden sm:block text-xl mr-2 ml-2">{navItem.name}</span>
+            </Link>
+          ))}
+        </div>
+
+        {/* Right Section - Login Button */}
+        <div className="ml-auto">
+          <button className="border text-sm font-medium relative bg-[#4E4E50] border-white rounded-md text-white px-4 py-2 rounded-full">
+            <span>Login</span>
+            <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
+          </button>
+        </div>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default FloatingNav;
